5e8d65ee300f3651dd5a9bb481ebc079327ba692
caoxy <sxinyus@126.com>
2018-03-09 14:10:58 +0800

    fix lag in windows(decrease the garbage-collect frequency)

--- cxy_config/init-better-defaults.el
+++ cxy_config/init-better-defaults.el
@@ -31,6 +31,15 @@
 
 ;;中文与外文字体设置, 保证中英文等宽等高目前只有使用文泉驿等宽正黑
 (set-default-font "文泉驿等宽正黑:pixelsize=18:foundry=unknown:weight=medium:slant=normal:width=normal:scalable=true")
+;; 设置垃圾回收，在Windows下，emacs25版本会频繁出发垃圾回收，所以需要设置
+(when (eq system-type 'windows-nt)
+  (setq gc-cons-threshold (* 512 1024 1024))
+  (setq gc-cons-percentage 0.5)
+  (run-with-idle-timer 5 t #'garbage-collect)
+  ;; 显示垃圾回收信息，这个可以作为调试用
+  ;; (setq garbage-collection-messages t)
+)
+
 ;; 加大行间距, 否则使用文泉驿等宽字体体验不佳
 (setq-default line-spacing 5)
 
